17a68155991f482d3d18544b88d37259bb163292
Thanh Nam <nam.uetn@gmail.com>
2020-05-09 17:04:15 +0700

    Fix bug

--- resources/views/pages/blocks/widgets/search_people_block.blade.php
+++ resources/views/pages/blocks/widgets/search_people_block.blade.php
@@ -5,7 +5,7 @@
             <img class="default-avatar" src="{{ getAvatar($user->avatar) }}" alt="{{$user->name}}">
         </div>
         <div class="notification-event">
-            <a href="{{ route('user.profile', $user->username) }} " class="h6 notification-friend sender">{{ $user->name }}</a>
+            <a href="{{ route('user.profile', $user->username) }} " class="h6 notification-friend sender">{{ strLimit($user->name, 20) }}</a>
 
             @if ($countMutuals = auth()->user()->countMutualFriends($user->id))
                 <span class="chat-message-item">
@@ -14,7 +14,7 @@
             @endif
 
         </div>
-        
+
         @if (auth()->user()->isFriends($user)->exists())
             <span class="notification-icon">
                 <img src="{{ asset('theme/socialyte/svg-icons/top_bar/friend.svg') }}">
